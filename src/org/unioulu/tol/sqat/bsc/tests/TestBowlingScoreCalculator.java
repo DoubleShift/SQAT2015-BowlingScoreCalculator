61a3a021b7cf8927923b16b8cfd5db70fcfaac39
somename <someemail>
2015-10-14 16:44:03 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
+++ src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
@@ -7,8 +7,34 @@ import org.junit.Test;
 public class TestBowlingScoreCalculator {
 
 	@Test
-	public void test() {
+	public void testBowlingGameaddFrame() {
 		fail("Not yet implemented");
 	}
+
+	@Test
+	public void testBowlingGamesetBonus() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testBowlingGamescore() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testBowlingGameisNextFrameBonus() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testBowlingGamegetNextThrow() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testBowlingGamegetNextTwoThrows() {
+		fail("Not yet implemented");
+	}
+	
 
 }
