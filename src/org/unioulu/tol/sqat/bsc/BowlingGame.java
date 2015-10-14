44f37a4a0be53f54ab89527b197b344f64bd8649
somename <someemail>
2015-10-14 16:42:08 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat/bsc/BowlingGame.java
+++ src/org/unioulu/tol/sqat/bsc/BowlingGame.java
@@ -7,8 +7,11 @@ public class BowlingGame {
 	//a bowling game is made of (at least) 10 frames
 	private List<Frame> frames = new ArrayList<Frame>();
 	private Frame bonus;
+	private int score;
 	
-	public BowlingGame(){}
+	public BowlingGame(){
+		this.score = 0;
+	}
 	
 	public void addFrame(Frame frame){
 		frames.add(frame);
@@ -18,21 +21,33 @@ public class BowlingGame {
 		Frame frame = new Frame(firstThrow,secondThrow);
 		this.addFrame(frame);
 		
+		//set lastFramflat
+		int size = frames.size();
+		if(size == 10){
+			frame.setLastFrame();
+		}
+		
 	}
 	
 	public int score(){
-		int score = 0;
-		
+		int tempScore = 0;
 		int len = frames.size();
 		for(int i =0 ; i<len;i++){
 			Frame frame = frames.get(i);
-			if(this.isNextFrameBonus(frame)){
-				
+			if(!frame.isLastFrame()){
+				if(frame.isSpare()){
+					frame.bonus(this.getNextThrow(i));
+				}else if(frame.isStrike()){
+					frame.bonus(this.getNextTwoThrows(i));
+				}
 			}else{
-				
+				//Todo tenth frame
 			}
+			
+			this.score = frame.score();
+			
 		}
-		return 0;
+		return this.score;
 	}
 	
 	
@@ -42,4 +57,23 @@ public class BowlingGame {
 		}
 		return false;
 	}
+	
+	public int getNextTwoThrows(int index){
+		int score = 0;
+		Frame frame = this.frames.get(index+1);
+		if(frame.getSecondThrow() != -1){
+			score = frame.getFirstThrow() + frame.getSecondThrow();
+		}else{
+			Frame nextFrame = this.frames.get(index+2);
+			score = frame.getFirstThrow()+nextFrame.getFirstThrow();
+		}
+		return score;
+	}
+	
+	public int getNextThrow(int index){
+		Frame frame = this.frames.get(index+1);
+
+		return frame.getFirstThrow();
+	}
+	
 }
